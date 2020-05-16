faa9b44001cb1127ed09f07bc18122b58e6d44e1
okada hatsune <swan.nilda1211@gmail.com>
2020-05-16 18:27:31 +0900

    Initial Commit

---
+++ Secret/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Secret
+//
+//  Created by Hatsune Okada on 5/16/20.
+//  Copyright © 2020 Okada Hattsunn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
